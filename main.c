0d9b01b87b34d4a185fbbc29313695ee27a898df
TLR18008637013 <TLR18008637013@outlook.com>
2024-10-08 23:32:13 +0800

    4

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+int compare(int a,int b){
+    int c;
+    if(a>b){
+        c=a;
+    }else{
+        c=b;
+    }
+    return c;
+}
 int main(){
-    printf("Hello TLR");
+    int a;
+    int b;
+    int compare(int a,int b);
+    printf("please write down the number of a:\n");
+    scanf("%d",&a);
+    printf("please write down the number of b:\n");
+    scanf("%d",&b);
+    int max;
+    max=compare(a, b);
+    printf("the bigger number is :%d",max);
     return 0;
 }
