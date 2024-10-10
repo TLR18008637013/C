eacfded92c441c4b0a7e7142e1d17033182a117c
TLR18008637013 <TLR18008637013@outlook.com>
2024-10-10 13:07:27 +0800

    4

--- main.c
+++ main.c
@@ -22,3 +22,49 @@ int main(){
     printf("the bigger number is :%d",max);
     return 0;
 }
+int main (){
+	int x,y;
+	for (x=1;x<=9;x++){
+		for(y=1;y<=9;y++){
+			printf("%d*%d=%-3d",x,y,x*y);
+		}printf("\n");
+	}
+	
+	return 0;
+} 
+int main(int argc, char *argv[]) {
+    intx,y;
+    for (x=1; x<=9; x++) {
+        for (y=x; y<=9; y++) {
+            printf("%d*%d=%-3d ",x,y,x*y);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+int main() {
+    int x,y,z;
+    for (x=1; x<=9; x++) {
+        for (z=1; z<x; z++) {
+        printf("        "); 
+        }
+        for (y=x; y<=9; y++) {
+        printf("%d*%d=%-3d ",x,y,x*y);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+int main(){
+	int x,y,z;
+	for(x=1;x<=9;x++){
+		for(z=9;z>=x;z--){
+		printf("       ");
+		}
+		for(y=1;y<=x;y++){
+		printf("%d*%d=%-3d",x,y,x*y);
+		}
+		printf("\n");
+	}
+	return 0;
+}
